2605beb6212627ad49435955902d57fd1b7df3c6
yassinelakchouch <yassinelakchouch@gmail.com>
2022-10-25 20:37:18 +0100

    .

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const SendEmail = async (fullName,email,message) => {
     try {
       const datas = {fullName, email, message };
-      let res = await axios.post(`http://localhost:8000/send`, datas);
+      let res = await axios.post(`https://omar-ahlal-backend.herokuapp.com/send`, datas);
     } catch (error) {
       alert(error.response.data.msg);
     }
